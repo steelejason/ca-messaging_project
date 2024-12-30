c08906cfa2e8f9193c900b0bb5054de2d9582519
steelejason <jsteelema@gmail.com>
2024-12-30 14:30:12 -0500

    added comedian object literal, created methods out of randomizer and joke output

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const jokes = [
-    ['Wafer', 'Been a wafer a while, but now I\'m back.'],
-    ['Gwen', 'Gwen will these jokes ever end?'],
-    ['Howard', 'Good. Howard you?'],
-    ['Mint', 'Mint to tell ya I was coming by.'],
-    ['Waddle', 'Waddle you give me to stop knocking?'],
-    ['Dublin', 'I\'m Dublin down on telling these knock-knock jokes.'],
-    ['Erin', 'Erin the tires is what my bike needs.'],
-    ['Ilene', 'Ilene a little when I get tired.'],
-    ['Pooch', 'Pooch your hat on, it\'s cold out.']
-];
+const comedian = {
+    name: 'Jason',
+    age: 38,
+    jokes: [
+        ['Wafer', 'Been a wafer a while, but now I\'m back.'],
+        ['Gwen', 'Gwen will these jokes ever end?'],
+        ['Howard', 'Good. Howard you?'],
+        ['Mint', 'Mint to tell ya I was coming by.'],
+        ['Waddle', 'Waddle you give me to stop knocking?'],
+        ['Dublin', 'I\'m Dublin down on telling these knock-knock jokes.'],
+        ['Erin', 'Erin the tires is what my bike needs.'],
+        ['Ilene', 'Ilene a little when I get tired.'],
+        ['Pooch', 'Pooch your hat on, it\'s cold out.']
+    ],
+    getRandomJoke: function() {
+        return Math.floor(Math.random() * 5);
+    },
+    tellJoke: function() {
+        joke = this.jokes[this.getRandomJoke()];
+        return `Knock Knock! Who's there? ${joke[0]}. ${joke[0]} who? ${joke[1]}`;
+    }
+};
 
-const getRandomJoke = function() {
-    return Math.floor(Math.random() * 5);
-}
+console.log(comedian.tellJoke());
 
-const tellJoke = function(joke) {
-    return `Knock Knock! Who's there? ${joke[0]}. ${joke[0]} who? ${joke[1]}`;
-}
 
-console.log(tellJoke(jokes[getRandomJoke()]));
