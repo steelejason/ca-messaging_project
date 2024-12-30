fcfb2ae7f98a0ac69902d33fffc1303837c339ba
steelejason <jsteelema@gmail.com>
2024-12-30 14:15:38 -0500

    reduced jokes to two parts down from three, modified joke telling output to accomodate. added +4 jokes.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const jokes = [
-    ['Wafer.', 'Wafer who?', 'Been a wafer a while, but now I\'m back.'],
-    ['Gwen.', 'Gwen who?', 'Gwen will these jokes ever end?'],
-    ['Howard.', 'Howard who?', 'Good. Howard you?'],
-    ['Mint.', 'Mint who?', 'Mint to tell ya I was coming by.'],
-    ['Waddle.', 'Waddle who?', 'Waddle you give me to stop knocking?']
+    ['Wafer', 'Been a wafer a while, but now I\'m back.'],
+    ['Gwen', 'Gwen will these jokes ever end?'],
+    ['Howard', 'Good. Howard you?'],
+    ['Mint', 'Mint to tell ya I was coming by.'],
+    ['Waddle', 'Waddle you give me to stop knocking?'],
+    ['Dublin', 'I\'m Dublin down on telling these knock-knock jokes.'],
+    ['Erin', 'Erin the tires is what my bike needs.'],
+    ['Ilene', 'Ilene a little when I get tired.'],
+    ['Pooch', 'Pooch your hat on, it\'s cold out.']
 ];
 
 const getRandomJoke = function() {
@@ -11,9 +15,7 @@ const getRandomJoke = function() {
 }
 
 const tellJoke = function(joke) {
-    return `Knock Knock! Who's there? ${joke[0]} ${joke[1]} ${joke[2]}`;
+    return `Knock Knock! Who's there? ${joke[0]}. ${joke[0]} who? ${joke[1]}`;
 }
 
 console.log(tellJoke(jokes[getRandomJoke()]));
-
-
